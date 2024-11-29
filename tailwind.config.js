172636d4e41a826537f57136ca76a807020b4ed0
malxcdev <malxcondev@gmail.com>
2024-11-28 23:29:08 -0500

    Recipe-Project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+        youngSerif: ['Young Serif', 'serif'],
+      },
+    },
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      stone: {
+        100: 'hsl(30, 54%, 90%)',
+        150: 'hsl(30, 18%, 87%)',
+        600: 'hsl(30, 10%, 34%)',
+        900: 'hsl(24, 5%, 18%)',
+      },
+      brown: {
+        800: 'hsl(14, 45%, 36%)',
+      },
+      rose: {
+        50: 'hsl(330, 100%, 98%)',
+        800: 'hsl(332, 51%, 32%)',
+      },
+    },
+  },
+  plugins: [],
+}
+
